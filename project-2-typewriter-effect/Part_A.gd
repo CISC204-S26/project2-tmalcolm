6707d2873ee53d1ff0f5ba98f633348fa20e7e80
Torian10 <tmalcolm@udel.edu>
2026-05-19 13:33:25 -0400

    Update Part_A.gd

--- project-2-typewriter-effect/Part_A.gd
+++ project-2-typewriter-effect/Part_A.gd
@@ -1,10 +1,27 @@
 extends RichTextLabel
 @export var dialoge = [
-	"I lay my head on the desk as the teacher does today's lecture.",
-	"I wasn't feeling like my best all week.",
-	"Assinments after assinments due the next day.",
-	"Also my family is getting on my last nerves.",
-	"How will this day get any worse?"
+	"Khole: Mom, why did you call this meeting?",
+	"Mom: Girls, im holding a meeting.",
+	"Khole: I literally just said that.",
+	"Mom: You three need to change.",
+	"Khortney: I like me tjust the way I am.",
+	"Mom: You're the oldest siblings and your are setting up a bad example.",
+	"Khole: Setting a bad exaple for who?",
+	"Mom: Kylie, Kendall, The other one.",
+	"Khorney: How?",
+	"Mom: Look at Kim, Look what she is doing right now.",
+	"Kim:...What?",
+	"Mom: What are you on the internet right now?",
+	"Kim: Nothing.",
+	"Mom: Shopping?",
+	"Kim: NO!",
+	"Kim: I'm looking at the president's incubation.",
+	"Mom: This is my point!",
+	"Mom: You girls are so used to having money and everything at your finger tips.",
+	"Mom: that you're lost touch with humanity.",
+	"Kholoe: Oh my god, fine.",
+	"Kholoe: Whats's her snapchat name?",
+	
 ]
 var index = 0 
 var string = ""
